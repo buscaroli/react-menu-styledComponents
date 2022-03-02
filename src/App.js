6562ae219f2af83dabfca9156db11460d615b064
mabus <m.buscaroli@outlook.com>
2022-03-02 19:18:56 +0000

    Fixed a CSS background bug

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { React, useState } from "react";
-import styled from "styled-components";
-import cities from "./data.js";
-import Title from "./Title";
-import Menu from "./Menu";
-import List from "./List";
+import { React, useState } from 'react'
+import styled from 'styled-components'
+import cities from './data.js'
+import Title from './Title'
+import Menu from './Menu'
+import List from './List'
 
 const StyledApp = styled.div`
   margin: 0 auto;
   padding: 0px;
   max-width: min(80%, 800px);
-  height: 100%;
-`;
+  min-height: 100vh;
+`
 
-let title = "Countries";
-const menuItems = ["All", ...new Set(cities.map((city) => city.country))];
+let title = 'Countries'
+const menuItems = ['All', ...new Set(cities.map((city) => city.country))]
 
 function App() {
-  const [data, setData] = useState(cities);
-  const [menu, setMenu] = useState(menuItems);
+  const [data, setData] = useState(cities)
+  const [menu, setMenu] = useState(menuItems)
 
   const filterItems = (country) => {
-    if (country === "All") {
-      setData(cities);
-      return;
+    if (country === 'All') {
+      setData(cities)
+      return
     }
-    const filteredItems = cities.filter((city) => city.country === country);
-    setData(filteredItems);
-  };
+    const filteredItems = cities.filter((city) => city.country === country)
+    setData(filteredItems)
+  }
 
   return (
     <StyledApp>
@@ -34,7 +34,7 @@ function App() {
       <Menu countries={menu} filterItems={filterItems} />
       <List data={data} />
     </StyledApp>
-  );
+  )
 }
 
-export default App;
+export default App
